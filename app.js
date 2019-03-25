a1c98de8ed42bfa4c3853b01566e62190356d6c6
Moe Terani <moe.terani@gmail.com>
2019-03-25 21:27:51 +0100

    Second initial commit

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector("#current-" + activePlayer).textContent = dice;
 
-// document.querySelector("#current-" + activePlayer).innerHTML =
-// "<em>" + dice + "</em>";
-
+/* document.querySelector("#current-" + activePlayer).innerHTML =
+ "<em>" + dice + "</em>";
+*/
 let getter = document.querySelector("#score-0").textContent;
 
 console.log(getter);
